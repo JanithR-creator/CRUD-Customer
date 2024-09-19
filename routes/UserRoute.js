72aec4eafbec2156b4cabb62e8ae795a7fc2033d
Janith Ranasinghe <janithraditha@gmail.com>
2024-09-19 21:06:39 +0530

    create user router

---
+++ routes/UserRoute.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controller/UserController');
+
+router.post('/signup', userController.signUp);
